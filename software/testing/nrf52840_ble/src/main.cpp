a3000ae52ba7d6de0e512930f5a5d1fb1f1662fc
davider3 <dreinhardt389@gmail.com>
2024-02-06 13:21:15 -0700

    ble not working

--- software/testing/nrf52840_ble/src/main.cpp
+++ software/testing/nrf52840_ble/src/main.cpp
@@ -1,18 +1,40 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+String sendMessage;
+String receivedMessage;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup(){
+  // SETUP UP INTERNAL LEDS
+  pinMode(11, OUTPUT); // RED
+  pinMode(12, OUTPUT); // GREEN
+  pinMode(13, OUTPUT); // BLUE
+  digitalWrite(11, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(13, HIGH);
+
+  // SETUP SERIAL MONITOR
+  Serial.begin(9600);
+  delay(25);
+
+  // SETUP UART COMMUNICATION WITH SERIAL
+  Serial1.begin(9600);
+  digitalWrite(11, LOW);
+  Serial.println("Connected");
+  delay(25);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop(){
+  while (Serial1.available() > 0) {
+    char receivedChar = Serial1.read();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    if (receivedChar == '\n') {
+      // Print the received message in the Serial monitor
+      Serial.println(receivedMessage);
+      // Reset the received message
+      receivedMessage = "";
+    } else {
+      // Append characters to the received message
+      receivedMessage += receivedChar;  
+    }
+  }
 }
--- software/testing/nrf52840_blink/src/main.cpp
+++ software/testing/nrf52840_blink/src/main.cpp
@@ -8,6 +8,6 @@ void setup(){
 
 void loop(){
   digitalWrite(11, LOW);
-  digitalWrite(12, HIGH);
+  digitalWrite(12, LOW);
   digitalWrite(13, HIGH);
 }
