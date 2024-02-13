22cb73ed240e98d593bcb71babe7eb48a286b37b
davider3 <dreinhardt389@gmail.com>
2024-02-13 15:27:13 -0700

    Joystick working

---
+++ software/testing/nrf52840_joystick/src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+
+#define X A2
+#define Y A1
+#define PUSH 3
+
+void setup() {
+  pinMode(X, INPUT);
+  pinMode(Y, INPUT);
+  pinMode(PUSH, INPUT);
+
+  // SETUP SERIAL MONITOR
+  Serial.begin(9600);
+  delay(100);
+  Serial.println("Serial Began");
+}
+
+void loop() {
+  Serial.print("X position: ");
+  Serial.println(analogRead(X));
+  Serial.print("Y position: ");
+  Serial.println(analogRead(Y));
+  Serial.print("Button: ");
+  Serial.println(digitalRead(PUSH));
+  delay(2000);
+}
